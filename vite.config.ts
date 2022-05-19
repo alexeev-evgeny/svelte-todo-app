bab3bf50cacbac226ae4250536a1c08b842e2382
Evgeny Alekseev <evgenij.alekseev@sbermegamarket.ru>
2022-05-19 17:12:11 +0700

    tmp

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import legacyPlugin from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(), 
+    legacyPlugin({
+      targets: ['IE >= 11']
+    })
+  ]
 })
